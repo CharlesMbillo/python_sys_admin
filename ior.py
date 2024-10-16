8b3d8acb46458ec2415c14b7348c54275bb50502
unknown <myapps255@gmail.com>
2024-10-16 11:03:59 +0300

    installs and uninstalls packages

---
+++ ior.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+
+def install_or_remove_packages():
+    iOrR= ""
+    while iOrR!= "I" and iOrR != "R":
+        print("Would you like to install orremove packages? (I/R)")
+        iOrR= input().upper()
+    if iOrR == "I":
+        iOrR= "install"
+    elif iOrR == "R":
+        iOrR= "remove"
+
+    print("Enter a list of packages to install")
+    print("The list should be separated by spaces, for example:")
+    print("package1 package2 package3")
+    print("Otherwise, input 'default' to " + iOrR + " the default packages listed in this program")
+    packages = input().lower()
+    if packages == "default":
+        packages = "defaultPackages"
+    
+    if iOrR == "install":
+        os.system("sudo yum install " + packages)
+
+    elif iOrR == "remove":
+        while True:
+            print("Purge files after removing? (Y/N)")
+            choice = input().upper()
+            if choice == "Y":
+                os.system("sudo yum --purge " +iOrR + " " + packages)
+                break
+            elif choice == "N":
+                os.system("sudo yum " + iOrR + "" + packages)
+                break
+        os.system("sudo yum autoremove")
+
+
