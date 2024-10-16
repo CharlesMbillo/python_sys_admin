1384353fbfece4653f1c35cec2ca23b8180def99
unknown <myapps255@gmail.com>
2024-10-16 12:01:52 +0300

    updates

--- ior.py
+++ ior.py
@@ -1,10 +1,12 @@
+#!/usr/bin/python3
+
 import os
 import subprocess
 
 def install_or_remove_packages():
     iOrR= ""
     while iOrR!= "I" and iOrR != "R":
-        print("Would you like to install orremove packages? (I/R)")
+        print("Would you like to install or remove packages? (I/R)")
         iOrR= input().upper()
     if iOrR == "I":
         iOrR= "install"
--- newuser.py
+++ newuser.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import os
 
 def add_user():
