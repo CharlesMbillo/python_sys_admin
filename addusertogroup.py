b8696397aec9d7319d8bf1a59fd3a4b74f3ffa2e
unknown <myapps255@gmail.com>
2024-10-16 12:50:52 +0300

    updates

--- addusertogroup.py
+++ addusertogroup.py
@@ -4,11 +4,11 @@ import os
 import subprocess
 
 def add_user_to_group():
-    username = input("Enter the name of the user that you wantto add to a group: ")
+    username = input("Enter the name of the user that you want to add to a group: ")
     output = subprocess.Popen('groups', stdout=subprocess.PIPE).communicate()[0]
     output = output.decode('utf-8')
     print("Enter a list of groups to add the user to")
-    print("The list should be separatedby spaces, for example:\r\n group1 group2 group3")
+    print("The list should be separated by spaces, for example:\r\n group1 group2 group3")
     print("The available groups are:\r\n " + output)
     chosenGroups = input("Groups: ")
     output = output.split(" ")
@@ -34,7 +34,7 @@ def add_user_to_group():
     groupString = groupString[:-1] + " "
     confirm = ""
     while confirm != "Y" and confirm != "N":
-        print("Add user " + username + " to thesegroups? (Y/N)")
+        print("Add user " + username + " to these groups? (Y/N)")
         confirm = input().upper()
     if confirm == "N":
         print("User '" + username + "' not added")
